fa0ddc1e6649b1e0fa9ce9c942028a0e363c04ed
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-03-21 15:17:49 +0300

    Completed assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,18 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    # set session initial value
+    # +1 session value, for every click
+    # if user.view< 3
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    
+    if session[:page_views] <= 3
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
